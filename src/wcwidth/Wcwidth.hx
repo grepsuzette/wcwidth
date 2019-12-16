d91c4b6372c90ba3838eba965ad5ea760b6e0e86
grepsuzette <emugel@qq.com>
2019-12-16 16:26:04 +0800

    still working on it

--- src/wcwidth/Wcwidth.hx
+++ src/wcwidth/Wcwidth.hx
@@ -275,7 +275,7 @@ class Wcwidth {
         var mid : Int;
         if (ucs < table[0].first || ucs > table[max].last) return 0;
         while (max >= min) {
-            mid = (min + max) / 2;
+            mid = Std.int( (min + max) / 2 );
             if (ucs > table[mid].last) min = mid + 1;
             else if (ucs < table[mid].first) max = mid - 1;
             else return 1;
