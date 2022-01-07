7f404ebd3d208e1306d5f68bda53b61917c1c92f
grepsuzette <emugel@qq.com>
2022-01-07 09:40:48 +0800

    add some ultra basic test

---
+++ test/Test.hx
@@ -0,0 +1,35 @@
+package test;
+
+using wcwidth.Wcwidth;
+
+class Test {
+    public static function assertEquals<T>(a:T, b:T) : Void {
+        if (a != b) throw "assertEquals failed: " 
+            + Std.string(a) + " differs from " + Std.string(b);
+    }
+
+    public static function utf8ToEcs2(s:String) : String {
+#if (!target.unicode)
+    #if sys
+         // TODO check if we are on linux or iconv (or uconv) exists
+         var pr = new sys.io.Process("iconv", ["-f", "UTF-8", "-t", "ECS-2"]);
+         pr.stdin.writeString(s, RawNative);
+         pr.stdin.close();
+         while ( null == pr.exitCode(true) ) Sys.sleep(0.0015);
+         var s = pr.stdout.readLine();
+         pr.close();
+         return s;
+    #else
+         #error "Target is not unicode ready yet, and we won't be able to use iconv because it's a non-sys platform"
+    #end
+#else
+         return s;
+#end
+    }
+
+    public static function main() {
+        trace("DONE");
+        assertEquals("你".code, 0x4f60);
+        assertEquals("你".charCodeAt(0), 0x4f60);
+    }
+}
